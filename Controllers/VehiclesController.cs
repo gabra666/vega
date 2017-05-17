3c70945452d1018d44ea89087d5a986a40c1f284
BRYAN-PC\Bryan <gabra666@hotmail.com>
2017-05-17 17:39:06 +0100

    CRUD api methods for vehicle

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -42,6 +42,12 @@ namespace VEGA.Controllers
                 return BadRequest(ModelState);
 
             var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
             mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
             vehicle.LastUpdate = DateTime.Now;
 
@@ -49,6 +55,35 @@ namespace VEGA.Controllers
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
             return Ok(result);
+        }
+
+        [HttpDeleteAttribute("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+            return Ok(id);
+        }
+
+        [HttpGetAttribute("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleResource = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(vehicleResource);
         }
 
 
